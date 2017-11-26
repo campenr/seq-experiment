f9ead950f38aeb0e346bdd9f728836f472916aea
Richard Campen <richard@campen.co>
2017-11-26 18:37:46 -0500

    Improve test data

--- tests/tests.py
+++ tests/tests.py
@@ -11,50 +11,78 @@ class Test(unittest.TestCase):
 
     def setUp(self):
 
+        # --------------- SETUP --------------- #
+
         # set random seed
         np.random.seed(123)
 
         # set data dimensions
-        feature_count = 11
-        class_count = 10
-        classification_count = 5
-        sample_data_count = 3
+        feature_count = 12  # should me multiple of 12
+        class_count = 12  # should me multiple of 12
+        classification_count = 4
+        metadata_count = 3
 
         # create data index's, columns
         features = ['feature_{}'.format(i) for i in range(feature_count)]
         classes = ['class_{}'.format(i) for i in range(class_count)]
         classification_ranks = ['rank_{}'.format(i) for i in range(classification_count)]
-        sample_data_names = ['sample_data_{}'.format(i) for i in range(sample_data_count)]
+        metadata_names = ['metadata_{}'.format(i) for i in range(metadata_count)]
 
         # create dummy feature table
-        self.test_feature_table = pd.DataFrame(
+        self.test_features = pd.DataFrame(
             np.random.randint(low=0, high=1000, size=(feature_count, class_count)),
             index=features,
             columns=classes
         )
 
-        # create dummy classification table
-        self.test_classification_table = pd.DataFrame(
-            np.random.randint(low=0, high=10, size=(feature_count, classification_count)),
+        # create dummy classification data
+        possible_ranks = 'abcdefghijklmnopqrstuvwxyz'
+        rank_levels = [1, 3, 6, 12]
+        reversed_classification_ranks = list(reversed(classification_ranks))
+
+        classification_dict = dict()
+        for num_rank in reversed(range(classification_count)):
+            rank_name = reversed_classification_ranks[num_rank]
+            rank_level = rank_levels[num_rank]
+            num_items = feature_count // rank_level
+            rank_items = possible_ranks[:num_items]
+            rank_data = ['%s_%s' % (rank, num_rank) for rank in sorted(rank_items * rank_level)]
+
+            classification_dict[rank_name] = rank_data
+
+        self.test_classifications = pd.DataFrame(
+            classification_dict,
             index=features,
             columns=classification_ranks
         )
 
-        self.test_metadata_table = pd.DataFrame(
-            np.random.randint(low=0, high=100, size=(class_count, sample_data_count)),
+        # create dummy metadata
+        self.test_metadata = pd.DataFrame(
+            np.random.randint(low=0, high=100, size=(class_count, metadata_count)),
             index=classes,
-            columns=sample_data_names
+            columns=metadata_names,
+            dtype=np.int
         )
+
+        # create metadata column for grouping samples
+        extra_metadata_name = 'metadata_%s' % metadata_count
+        extra_metadatum = 'abc' * (class_count // 3)
+        extra_metadatum = [i for i in sorted(extra_metadatum)]
+        self.test_metadata[extra_metadata_name] = extra_metadatum
 
         # setup container to hold test SeqExp objects. They are actually created in the first few test functions
         self.test_sxps = dict()
+
+        # self.test_features = None
+        # self.test_classifications = None
+        # self.test_metadata = None
 
     def test_create_seq_experiment_1(self):
         """Test creating SeqExp with only feature_table."""
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table
+                features=self.test_features
             )
 
             # save this SeqExp to the test object for use by other test functions
@@ -68,8 +96,8 @@ class Test(unittest.TestCase):
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table,
-                classifications=self.test_classification_table
+                features=self.test_features,
+                classifications=self.test_classifications
             )
 
             # save this SeqExp to the test object for use by other test functions
@@ -83,8 +111,8 @@ class Test(unittest.TestCase):
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table,
-                metadata=self.test_metadata_table
+                features=self.test_features,
+                metadata=self.test_metadata
             )
 
             # save this SeqExp to the test object for use by other test functions
@@ -98,9 +126,9 @@ class Test(unittest.TestCase):
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table,
-                classifications=self.test_classification_table,
-                metadata=self.test_metadata_table
+                features=self.test_features,
+                classifications=self.test_classifications,
+                metadata=self.test_metadata
             )
 
             # save this SeqExp to the test object for use by other test functions
@@ -113,9 +141,9 @@ class Test(unittest.TestCase):
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table,
-                classifications=self.test_classification_table,
-                metadata=self.test_metadata_table
+                features=self.test_features,
+                classifications=self.test_classifications,
+                metadata=self.test_metadata
             )
 
             new_names = ['%sx' % old_name for old_name in seq_exp.sample_names]
@@ -128,9 +156,9 @@ class Test(unittest.TestCase):
 
         try:
             seq_exp = SeqExp(
-                features=self.test_feature_table,
-                classifications=self.test_classification_table,
-                metadata=self.test_metadata_table
+                features=self.test_features,
+                classifications=self.test_classifications,
+                metadata=self.test_metadata
             )
 
             new_names = ['%sx' % old_name for old_name in seq_exp.feature_names]
@@ -143,9 +171,9 @@ class Test(unittest.TestCase):
     def test_ordination_with_distance(self):
 
         seq_exp = SeqExp(
-            features=self.test_feature_table,
-            classifications=self.test_classification_table,
-            metadata=self.test_metadata_table
+            features=self.test_features,
+            classifications=self.test_classifications,
+            metadata=self.test_metadata
         )
 
         try:
@@ -249,22 +277,6 @@ class Test(unittest.TestCase):
             except Exception as e:
                 self.fail('Failed on %s.' % name, e)
 
-    # TODO implement this
-    # def test_indexing_6(self):
-    # """
-    # Tests setting column by attribute.
-    #
-    # https://pandas.pydata.org/pandas-docs/stable/indexing.html#attribute-access
-    #
-    # """
-    #
-    # # test on all SeqExp permutations
-    # for name, seq_exp in self.test_sxps.items():    #
-    #     try:
-    #         seq_exp.class_0 = seq_exp.class_0.feature_table * 2
-    #     except Exception as e:
-    #         self.fail('Failed on %s.' % name, e)
-
     def test_indexing_7(self):
         """
         Tests indexing by slice range.
@@ -280,30 +292,6 @@ class Test(unittest.TestCase):
                 seq_exp[::-1]
             except Exception as e:
                 self.fail('Failed on %s.' % name, e)
-
-    # def test_indexing_8(self):
-    #     """
-    #     Tests setting rows by slice range.
-    #
-    #     https://pandas.pydata.org/pandas-docs/stable/indexing.html#slicing-ranges
-    #
-    #     """
-    #
-    # # test on all SeqExp permutations
-    # for name, seq_exp in self.test_sxps.items():
-    #     try:
-    #         # passing feature table directly
-    #         seq_exp[:3] = seq_exp[:3].features * 2
-    #
-    #         # passing SeqExp directly
-    #         new_features = seq_exp[:3] = seq_exp[:3].features * 2
-    #         new_seq_exp = SeqExp(features=new_features, classifications=seq_exp.classifications,
-    #                              metadata=seq_exp.metadata)
-    #
-    #         seq_exp[:3] = seq_exp[:3]
-    #
-    #     except Exception as e:
-    #         self.fail('Failed on %s.' % name, e)
 
     # --------- test convenience functions --------- #
 
