7a4f4ef36057cccff0268092f1a0618b9133a251
Richard Campen <richard@campen.co>
2018-01-03 20:55:28 -0500

    Update setup.py for version 0.1.0

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-"""setup.py for mothur_py."""
+"""setup.py for seq-experiment."""
 
 from setuptools import setup, find_packages
 
-# # Get the long description from the README.md, converted to .rst if possible
-# try:
-#     import pypandoc
-#     long_description = pypandoc.convert('README.md', 'rst')
-#     print('using README.rst for long description')
-# except(IOError, ImportError):
-#     long_description = open('README.md').read()
-#     print('using README.md for long description')
+# Get the long description from the README.md, converted to .rst if possible
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    print('using README.rst for long description')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+    print('using README.md for long description')
 
 setup(
     name="seq-experiment",
@@ -18,10 +18,18 @@ setup(
     author_email="richard@campen.co",
     license="Modified BSD License",
 
-    # classifiers=[
-    # ],
-    #
-    # keywords="",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+
+    keywords="sequence bioinformatics",
     packages=find_packages(),
     include_package_data=True
 )
